0f4cc12f3291b09e832ba4d8d9e2c92e5f1bd1f7
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-11-16 16:37:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,69 +26,9 @@ client.user.setGame(`_Mr_FoFo🍻|_I_am_Strong<_*🍺`,'https://www.twitch.tv/fo
 
 
 
-client.on('message', message => {
-    if (message.content === 'فوفو') {
-        message.reply('عيوني');
-      }
-});
 
 
 
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-if (command == "embed") {
-    let say = new Discord.RichEmbed()
-  .setThumbnail(message.author.avatarURL)
-  .setAuthor(message.author.username)
-    .setDescription(args.join("  "))
-    .setColor(0x06DF00)
-    message.channel.sendEmbed(say);
-    message.delete();
-  }
-  });
-
-
-   client.on("message", message => {
-    const prefix = "F"
-
-          if(!message.channel.guild) return;
-   if(message.author.bot) return;
-      if(message.content === prefix + "image"){
-          const embed = new Discord.RichEmbed()
-
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
-  .setAuthor(message.author.username, message.guild.iconrURL)
-    .setColor(0x164fe3)
-    .setImage(message.guild.iconURL)
-    .setURL(message.guild.iconrURL)
-                    .setTimestamp()
-
-   message.channel.send({embed});
-      }
-  });
-
-  client.on('message', message => {
-    if (message.content.startsWith("Favatar")) {
-        var mentionned = message.mentions.users.first();
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
-	     }
-  });
 
 
 
