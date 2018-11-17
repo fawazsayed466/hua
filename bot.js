7f4fb13b29d5cf0af8f99ede18f40f1c475962e2
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-11-17 14:59:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ client.on('ready', () => {
 
 
 client.on('ready', () => {                           
-client.user.setGame(`🍸©мR_ƒØƒØ_مِسًـتُـر فُـوٌفُـوٌ©🍸`,'https://www.twitch.tv/fofodiscord');                                                                                                                                                                                                                                                                                                                                                                                                                            
+client.user.setGame(`🍸мR_ƒØƒØ_مِسًـتُـر_فُـوٌفُـوٌ🍸`,'https://www.twitch.tv/fofodiscord');                                                                                                                                                                                                                                                                                                                                                                                                                            
 });
 
 
