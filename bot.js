811ba65758daa0fe62b9e22b0847280d02ff6ac3
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-11-17 14:55:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,32 +28,6 @@ client.user.setGame(`♪-🍸≥≤©мR_ƒØƒØ_مِسًـتُـر فُـوٌ�
 
 
 
-const developers = ["513267781211389952",""]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'sp')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/fofodiscord");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
-
 
 
 client.login(process.env.BOT_TOKEN);
