02f545c7f3927e6ea00aa0ea9bf2baba813e4eb7
Hacker <42048326+fawazsayed466@users.noreply.github.com>
2018-11-25 02:27:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,24 +7,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["382889731316514826",""]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + '`✅   ${argresult}`**تم التحويل الحالة إلى**')) {
-    client.user.setGame(argresult);
-      message.channel.send(``)
-  } else  
-      if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)//fofo©
-  } else
-if (message.content.startsWith(adminprefix + 'setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);  
-}
-});
+
 
 
 
